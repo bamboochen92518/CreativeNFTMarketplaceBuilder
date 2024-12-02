43ab73ed65cb36ff57e08dbd90c236d13d2fe211
bamboo_chen <b10902005@ntu.edu.tw>
2024-12-02 18:01:06 +0800

    add: test regrade and bid

--- test/CharacterNFT.t.sol
+++ test/CharacterNFT.t.sol
@@ -10,6 +10,9 @@ import {CharacterNFT} from "src/CharacterNFT.sol";
 contract CharacterNFTTest is Test {
     address internal user;
     address internal owner;
+    address internal bidder0;
+    address internal bidder1;
+    address internal bidder2;
 
     CharacterNFT internal characterNFTContract;
 
@@ -20,15 +23,21 @@ contract CharacterNFTTest is Test {
         // Role
         user = makeAddr("user");
         owner = makeAddr("owner");
+        bidder0 = makeAddr("bidder0");
+        bidder1 = makeAddr("bidder1");
+        bidder2 = makeAddr("bidder2");
 
         vm.startPrank(owner);
         characterNFTContract = new CharacterNFT();
         vm.stopPrank();
 
-        vm.deal(user, 50 ether);
     }
 
     function testUploadCharacter() public {
+        // Give user 50 ether
+        vm.deal(user, 50 ether);
+
+        // User want to mint a NFT
         vm.startPrank(user);
         string memory image = '123hihi';
         characterNFTContract.uploadCharacter{value: UPLOAD_FEE}(user, image);
@@ -36,5 +45,52 @@ contract CharacterNFTTest is Test {
         assertEq(characterNFTContract.ownerOf(0), address(user));
         assertEq(characterNFTContract.characterCounter(), 1);
         vm.stopPrank();
+    }
+
+    function testRegradeCharacter() public {
+        // Base on testUploadCharacter()
+        testUploadCharacter();
+
+        // Other people want to regrade NFT => revert
+        vm.startPrank(bidder0);
+        vm.expectRevert();
+        characterNFTContract.regradeCharacter{value: REGRADE_FEE}(0);
+        vm.stopPrank();
+
+        // User want to regrade a NFT for 3 times
+        vm.startPrank(user);
+        for (uint256 i = 0; i < 3; i++) {
+            characterNFTContract.regradeCharacter{value: REGRADE_FEE}(0);
+            assertEq(address(user).balance, (35 - i * 5) * 1 ether);
+        }
+        vm.stopPrank();
+
+        // User want to regrade more than 3 times => revert
+        vm.startPrank(user);
+        vm.expectRevert();
+        characterNFTContract.regradeCharacter{value: REGRADE_FEE}(0);
+        vm.stopPrank();
+    }
+
+    function testBidCharacter() public {
+        // Base on testUploadCharacter()
+        testUploadCharacter();
+
+        // Give some token to bidder
+        vm.deal(bidder0, 20 ether);
+        vm.deal(bidder1, 20 ether);
+        vm.deal(bidder2, 20 ether);
+        
+        // Bidder want to buy NFT0
+        vm.prank(bidder0);
+        characterNFTContract.bidCharacter{value: 5 ether}(0);
+        vm.prank(bidder1);
+        characterNFTContract.bidCharacter{value: 15 ether}(0);
+        vm.prank(bidder2);
+        characterNFTContract.bidCharacter{value: 10 ether}(0);
+
+        assertEq(address(bidder0).balance, 15 ether);
+        assertEq(address(bidder1).balance, 5 ether);
+        assertEq(address(bidder2).balance, 10 ether);
     }
 }
